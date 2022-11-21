765b8a8b3fa069cc668feeccc5d3201a3b1fa26e
Erik Santana Marques dos Santos <erik.marques@mosaico.com.br>
2022-11-21 17:09:03 -0300

    a

--- src/services/api.ts
+++ src/services/api.ts
@@ -22,5 +22,8 @@ const api = axios.create({
 });
 
 api.defaults.withCredentials = true;
+api.defaults.headers['Access-Control-Allow-Origin'] = isDev
+	? 'http://localhost:8080'
+	: `https://www.playout.network`;
 
 export { api };
